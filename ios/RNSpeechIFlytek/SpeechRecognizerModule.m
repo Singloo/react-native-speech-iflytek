1d43efa21c4ca9fd64857030f83f8e2b83a5a0ef
Rick yang Tongxue <singloo.yang@gmail.com>
2019-07-23 17:19:35 +0800

    fix asr audio path

--- ios/RNSpeechIFlytek/SpeechRecognizerModule.m
+++ ios/RNSpeechIFlytek/SpeechRecognizerModule.m
@@ -78,9 +78,6 @@ RCT_EXPORT_METHOD(stop) {
 
 RCT_EXPORT_METHOD(setParameter: (NSString *) parameter
                   value: (NSString *) value) {
-    if ([parameter isEqualToString: IFlySpeechConstant.ASR_AUDIO_PATH]) {
-        value = [self getAbsolutePath: value];
-    }
     [self.iFlySpeechRecognizer setParameter: value forKey: parameter];
 }
 
